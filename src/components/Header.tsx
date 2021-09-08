25d3764e66d389e7878dc7c49a51112c863c1cd8
Felipe Vitoria <felipervitoria@hotmail.com>
2021-09-08 09:25:53 -0300

    worldtrip full version v4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,42 +2,54 @@ import { Flex, Image } from '@chakra-ui/react'
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ChevronLeftIcon } from '@chakra-ui/icons'
+import Head from 'next/head';
+
 
 export function Header() {
   const { asPath } = useRouter()
 
   if (asPath !== '/') {
     return (
-      <Flex
-        w="100%"
-        as="header"
-        maxWidth={1480}
-        h="20"
-        mx="auto"
-        mt="4"
-        align="center"
-        px="1"
-      >
-        <Link href="/" >
-          <ChevronLeftIcon fontSize="3xl" _hover={{ "cursor": "pointer" }} />
-        </Link>
-        <Image m="auto" src="/images/Logoworld.svg" alt="Logo World Trip" />
-      </Flex>
+      <>
+        <Head>
+          <title>Posts | Ignews</title>
+        </Head>
+        <Flex
+          w="100%"
+          as="header"
+          maxWidth={1480}
+          h="20"
+          mx="auto"
+          mt="4"
+          align="center"
+          px="1"
+        >
+          <Link href="/" >
+            <ChevronLeftIcon fontSize="3xl" _hover={{ "cursor": "pointer" }} />
+          </Link>
+          <Image m="auto" src="/images/Logoworld.svg" alt="Logo World Trip" />
+        </Flex>
+      </>
     );
   } {
     return (
-      <Flex
-        w="100%"
-        as="header"
-        maxWidth={1480}
-        h="20"
-        mx="auto"
-        mt="4"
-        align="center"
-        px="1"
-      >
-        <Image m="auto" src="/images/Logoworld.svg" alt="Logo World Trip" />
-      </Flex>
+      <>
+        <Head>
+          <title>Posts | Ignews</title>
+        </Head>
+        <Flex
+          w="100%"
+          as="header"
+          maxWidth={1480}
+          h="20"
+          mx="auto"
+          mt="4"
+          align="center"
+          px="1"
+        >
+          <Image m="auto" src="/images/Logoworld.svg" alt="Logo World Trip" />
+        </Flex>
+      </>
     );
   }
 }
